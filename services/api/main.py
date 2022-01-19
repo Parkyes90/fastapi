e217126acd2a9a6d62cffc4a2ebc0ff59bb4491c
parkyes90 <parkyes90@gmail.com>
2022-01-19 23:51:11 +0900

    port binding

--- services/api/main.py
+++ services/api/main.py
@@ -5,10 +5,10 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return "Hello world"
+    return "Hello world123"
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, port=8000, host="0.0.0.0")
