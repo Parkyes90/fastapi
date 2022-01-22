07e316f814e637f8b0a4ac3a34edd6f8d8145a33
parkyes90 <parkyes90@gmail.com>
2022-01-22 20:58:35 +0900

    with query parameters

--- services/api/main.py
+++ services/api/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from enum import Enum
 
@@ -15,19 +17,31 @@ def index():
     return {"message": "Hello World"}
 
 
-@app.get("/blog/all")
-def get_all_blogs():
-    return {"message": "All blogs"}
+@app.get("/blogs/{blog_id}/comments/{comment_id}")
+def get_comment(
+    blog_id: int,
+    comment_id: int,
+    valid: bool = True,
+    username: Optional[str] = None,
+):
+    return {
+        "message": f"blog_id {blog_id}, comment_id {comment_id}, valid {valid}, username: {username}"
+    }
 
 
-@app.get("/blog/type/{blog_type}")
+@app.get("/blogs/type/{blog_type}")
 def get_blog_type(blog_type: BlogType):
     return {"message": f"Blog type {blog_type}"}
 
 
-@app.get("/blog/{blog_id}")
+@app.get("/blogs/{blog_id}")
 def get_blog(blog_id: int):
     return {"message": f"Blog with id {blog_id}"}
+
+
+@app.get("/blogs")
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {"message": f"All {page_size} blogs on page {page}"}
 
 
 if __name__ == "__main__":
