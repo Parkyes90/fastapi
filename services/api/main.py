51b15725bcf91639b2d3247bb1cd725f7a7cca64
parkyes90 <parkyes90@gmail.com>
2022-01-23 18:25:37 +0900

    with tags

--- services/api/main.py
+++ services/api/main.py
@@ -18,7 +18,7 @@ def index():
     return {"message": "Hello World"}
 
 
-@app.get("/blogs/{blog_id}/comments/{comment_id}")
+@app.get("/blogs/{blog_id}/comments/{comment_id}", tags=["blogs", "comments"])
 def get_comment(
     blog_id: int,
     comment_id: int,
@@ -30,12 +30,12 @@ def get_comment(
     }
 
 
-@app.get("/blogs/type/{blog_type}")
+@app.get("/blogs/type/{blog_type}", tags=["blogs"])
 def get_blog_type(blog_type: BlogType):
     return {"message": f"Blog type {blog_type}"}
 
 
-@app.get("/blogs/{blog_id}", status_code=status.HTTP_200_OK)
+@app.get("/blogs/{blog_id}", status_code=status.HTTP_200_OK, tags=["blogs"])
 def get_blog(blog_id: int, response: Response):
     if blog_id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -43,7 +43,7 @@ def get_blog(blog_id: int, response: Response):
     return {"message": f"Blog with id {blog_id}"}
 
 
-@app.get("/blogs")
+@app.get("/blogs", tags=["blogs"])
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {"message": f"All {page_size} blogs on page {page}"}
 
