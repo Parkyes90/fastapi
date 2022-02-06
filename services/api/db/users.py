add546e91e6566289e23258092247ecc008c4f18
parkyes90 <parkyes90@gmail.com>
2022-02-06 13:13:21 +0900

    Add User helper for create user

---
+++ services/api/db/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+
+from db.models import User
+from schemas.users import UserBase
+from db.utils import PasswordHash
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = User(
+        username=request.username,
+        email=request.email,
+        password=PasswordHash(request.password).bcrypt(),
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
